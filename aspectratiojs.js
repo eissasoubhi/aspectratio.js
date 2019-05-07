3c4d85f459c87a686f7f0137a2012a3d4091ce06
Eissa Soubhi <eissa.soubhi@gmail.com>
2019-05-07 06:09:54 +0000

    Fix bug and add comments

--- aspectratiojs.js
+++ aspectratiojs.js
@@ -1,38 +1,56 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-	if(jQuery)
-	{
-		var full_responsive = $('.aspectratio'),
-	    ar_width = full_responsive.data('fr-width'),
-	    ar_height = full_responsive.data('fr-height'),
-	    fluid = full_responsive.data('fr-fluid') || true,
-	    aspect_ratio = ar_height/ar_width,
-	    current_width, responsive_height,
-	    __resize,
-	    css = {
-	        "box-sizing" : 'border-box',
-	    };
+    if(jQuery)
+    {
+        var element = $('.aspectratio'),
 
-	    if(fluid)
-	    {
-	        css["max-width"] = ar_width,
-	        css["max-height"] = ar_height
-	    }
+        // aspect ratio width
+        ar_width = element.data('ar-width'),
 
-	    (__resize = function () {
-	        current_width = full_responsive.outerWidth();
-	        responsive_height = current_width * aspect_ratio;
+        // aspect ratio height
+        ar_height = element.data('ar-height'),
 
-	        css["height"] = responsive_height;
+        // whether the element is taking its parent full width
+        full_width = element.data('ar-full_width') == undefined || element.data('ar-full_width') == true,
 
-	        full_responsive.css(css);
-	    })();
+        // calculating aspect ratio factor
+        aspect_ratio_factor = ar_height/ar_width,
 
-	    $(window).resize(function(event) {
-	        __resize();
-	    });
-	}
-	else
-	{
-		console.error('aspectratio requires jQuery to work');
-	}
+        current_width, responsive_height,
+
+        __resize,
+
+        css = {};
+
+        if( ! full_width)
+        {
+            // fix the element's maximum size to the given aspect ratio width and height
+            css["max-width"] = ar_width,
+            css["max-height"] = ar_height
+        }
+
+        element.css({
+            "box-sizing" : 'border-box',
+            // aspect ratio works only on block display elements
+            "display" : 'block',
+        });
+
+        (__resize = function () {
+            current_width = element.outerWidth();
+
+            // calculate the aspect ratio height
+            responsive_height = current_width * aspect_ratio_factor;
+
+            css["height"] = responsive_height;
+
+            element.css(css);
+        })();
+
+        $(window).resize(function(event) {
+            __resize();
+        });
+    }
+    else
+    {
+        console.error('aspectratio requires jQuery to work');
+    }
 });
